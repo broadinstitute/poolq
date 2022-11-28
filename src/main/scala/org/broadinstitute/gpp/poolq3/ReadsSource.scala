0aa6000540c47cf1e16f14598e3de9e9c9954175
Mark Tomko <senzasord@gmail.com>
2022-11-28 12:06:02 -0800

    Add ReadsSource

---
+++ src/main/scala/org/broadinstitute/gpp/poolq3/ReadsSource.scala
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2022 The Broad Institute, Inc. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+package org.broadinstitute.gpp.poolq3
+
+import java.nio.file.Path
+
+import cats.data.{NonEmptyList => Nel}
+
+sealed trait ReadsSource extends Product with Serializable
+
+object ReadsSource {
+
+  final case class SelfContained(path: Nel[Path]) extends ReadsSource
+  final case class Split(index: Nel[Path], forward: Nel[Path]) extends ReadsSource
+  final case class PairedEnd(index: Nel[Path], forward: Nel[Path], reverse: Nel[Path]) extends ReadsSource
+
+}
