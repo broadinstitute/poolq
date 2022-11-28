8c5fec3faec08c4e77a35bd07261828dc280159e
Mark Tomko <senzasord@gmail.com>
2022-11-28 12:05:51 -0800

    Add a general exception type

---
+++ src/main/scala/org/broadinstitute/gpp/poolq3/types/PoolQException.scala
@@ -0,0 +1,9 @@
+/*
+ * Copyright (c) 2022 The Broad Institute, Inc. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+package org.broadinstitute.gpp.poolq3.types
+
+/** General exception raised by PoolQ */
+final case class PoolQException(msg: String) extends RuntimeException(msg)
