8bce0e0c6920f318eae64ca469f4d12e63c31456
Mark Tomko <senzasord@gmail.com>
2022-12-02 13:38:07 -0800

    Use ReadsSource and aggregate iterable

--- src/main/scala/org/broadinstitute/gpp/poolq3/barcode/package.scala
+++ src/main/scala/org/broadinstitute/gpp/poolq3/barcode/package.scala
@@ -9,6 +9,7 @@ import java.nio.file.Path
 
 import scala.collection.mutable
 
+import org.broadinstitute.gpp.poolq3.ReadsSource
 import org.broadinstitute.gpp.poolq3.parser.{CloseableIterable, CloseableIterator, FastqParser, SamParser, TextParser}
 import org.broadinstitute.gpp.poolq3.types.{BamType, FastqType, Read, ReadsFileType, SamType, TextType}
 
@@ -21,39 +22,31 @@ package object barcode {
     colBarcodePolicy: BarcodePolicy,
     umiBarcodePolicyOpt: Option[BarcodePolicy]
   ): CloseableIterable[Barcodes] =
-    (config.rowReads, config.reverseRowReads, config.colReads, config.reads) match {
-      case (Some(row), None, Some(col), _) =>
+    (config.readsSource, revRowBarcodePolicyOpt) match {
+      case (ReadsSource.Split(index, forward), None) =>
         new TwoFileBarcodeSource(
-          parserFor(row),
-          parserFor(col),
+          parserFor(forward.toList),
+          parserFor(index.toList),
           rowBarcodePolicy,
           colBarcodePolicy,
           umiBarcodePolicyOpt,
           config.readIdCheckPolicy
         )
-
-      case (Some(row), Some(revRow), Some(col), _) =>
-        revRowBarcodePolicyOpt match {
-          case None =>
-            throw new IllegalArgumentException("Paired end sequencing mode requires a reverse barcode policy")
-          case Some(revRowBarcodePolicy) =>
-            new ThreeFileBarcodeSource(
-              parserFor(row),
-              parserFor(revRow),
-              parserFor(col),
-              rowBarcodePolicy,
-              revRowBarcodePolicy,
-              colBarcodePolicy,
-              umiBarcodePolicyOpt,
-              config.readIdCheckPolicy
-            )
-        }
-
-      case (None, None, None, Some(reads)) =>
-        new SingleFileBarcodeSource(parserFor(reads), rowBarcodePolicy, colBarcodePolicy, umiBarcodePolicyOpt)
-
+      case (ReadsSource.PairedEnd(index, forward, reverse), Some(revRowBarcodePolicy)) =>
+        new ThreeFileBarcodeSource(
+          parserFor(forward.toList),
+          parserFor(reverse.toList),
+          parserFor(index.toList),
+          rowBarcodePolicy,
+          revRowBarcodePolicy,
+          colBarcodePolicy,
+          umiBarcodePolicyOpt,
+          config.readIdCheckPolicy
+        )
+      case (ReadsSource.SelfContained(paths), None) =>
+        new SingleFileBarcodeSource(parserFor(paths.toList), rowBarcodePolicy, colBarcodePolicy, umiBarcodePolicyOpt)
       case _ =>
-        throw new IllegalArgumentException("Either reads or row and column reads files must be specified")
+        throw new IllegalArgumentException("Incompatible reads and barcode policy settings")
     }
 
   def parserFor(file: Path): CloseableIterable[Read] =
@@ -64,39 +57,40 @@ package object barcode {
       case None                          => throw new IllegalArgumentException(s"File $file is of an unknown file type")
     }
 
-  def parserFor(files: List[Path]): CloseableIterable[Read] = {
+  def parserFor(files: List[Path]): CloseableIterable[Read] =
     parserFor[Path, Read](files, p => parserFor(p).iterator)
-  }
-  
-  private[barcode] def parserFor[A, B](sources: List[A], mkIterator: A => CloseableIterator[B]): CloseableIterable[B] = new CloseableIterable[B] {
-    override def iterator: CloseableIterator[B] = new CloseableIterator[B] {
 
-      private val queue: mutable.Queue[A] = mutable.Queue.from(sources)
+  private[barcode] def parserFor[A, B](sources: List[A], mkIterator: A => CloseableIterator[B]): CloseableIterable[B] =
+    new CloseableIterable[B] {
 
-      var current: CloseableIterator[B] = _
+      override def iterator: CloseableIterator[B] = new CloseableIterator[B] {
 
-      override def hasNext: Boolean = {
-        var currentHasNext = if (current == null) false else current.hasNext
-        while (!currentHasNext && queue.nonEmpty) {
-          val head = queue.dequeue()
-          if (head != null) {
-            val old = current
-            current = mkIterator(head)
-            if (old != null) { 
-              old.close()
+        private val queue: mutable.Queue[A] = mutable.Queue.from(sources)
+
+        var current: CloseableIterator[B] = _
+
+        override def hasNext: Boolean = {
+          var currentHasNext = if (current == null) false else current.hasNext
+          while (!currentHasNext && queue.nonEmpty) {
+            val head = queue.dequeue()
+            if (head != null) {
+              val old = current
+              current = mkIterator(head)
+              if (old != null) {
+                old.close()
+              }
+              currentHasNext = current.hasNext
             }
-            currentHasNext = current.hasNext
           }
+          currentHasNext
         }
-        currentHasNext
+
+        override def next(): B = if (current == null) throw new NoSuchElementException else current.next()
+
+        override def close(): Unit = Option(current).foreach(_.close())
+
       }
 
-      override def next(): B = if (current == null) throw new NoSuchElementException else current.next()
-
-      override def close(): Unit = Option(current).foreach(_.close())
-
     }
-
-  }
 
 }
