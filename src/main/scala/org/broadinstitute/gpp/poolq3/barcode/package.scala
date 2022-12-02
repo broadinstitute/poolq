81f95ff28da8e9a93c1d70d04e57a513f82c8aae
Mark Tomko <senzasord@gmail.com>
2022-12-02 13:27:40 -0800

    Aggregate iteratable implementation

--- src/main/scala/org/broadinstitute/gpp/poolq3/barcode/package.scala
+++ src/main/scala/org/broadinstitute/gpp/poolq3/barcode/package.scala
@@ -7,7 +7,9 @@ package org.broadinstitute.gpp.poolq3
 
 import java.nio.file.Path
 
-import org.broadinstitute.gpp.poolq3.parser.{CloseableIterable, FastqParser, SamParser, TextParser}
+import scala.collection.mutable
+
+import org.broadinstitute.gpp.poolq3.parser.{CloseableIterable, CloseableIterator, FastqParser, SamParser, TextParser}
 import org.broadinstitute.gpp.poolq3.types.{BamType, FastqType, Read, ReadsFileType, SamType, TextType}
 
 package object barcode {
@@ -61,5 +63,40 @@ package object barcode {
       case Some(TextType)                => new TextParser(file)
       case None                          => throw new IllegalArgumentException(s"File $file is of an unknown file type")
     }
+
+  def parserFor(files: List[Path]): CloseableIterable[Read] = {
+    parserFor[Path, Read](files, p => parserFor(p).iterator)
+  }
+  
+  private[barcode] def parserFor[A, B](sources: List[A], mkIterator: A => CloseableIterator[B]): CloseableIterable[B] = new CloseableIterable[B] {
+    override def iterator: CloseableIterator[B] = new CloseableIterator[B] {
+
+      private val queue: mutable.Queue[A] = mutable.Queue.from(sources)
+
+      var current: CloseableIterator[B] = _
+
+      override def hasNext: Boolean = {
+        var currentHasNext = if (current == null) false else current.hasNext
+        while (!currentHasNext && queue.nonEmpty) {
+          val head = queue.dequeue()
+          if (head != null) {
+            val old = current
+            current = mkIterator(head)
+            if (old != null) { 
+              old.close()
+            }
+            currentHasNext = current.hasNext
+          }
+        }
+        currentHasNext
+      }
+
+      override def next(): B = if (current == null) throw new NoSuchElementException else current.next()
+
+      override def close(): Unit = Option(current).foreach(_.close())
+
+    }
+
+  }
 
 }
