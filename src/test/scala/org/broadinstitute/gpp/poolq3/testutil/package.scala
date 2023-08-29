49b739408cf49373f86e5a57ca47d6a6092d0436
Mark Tomko <mtomko@broadinstitute.org>
2023-08-29 08:00:53 -0700

    Update context bounds

--- src/test/scala/org/broadinstitute/gpp/poolq3/testutil/package.scala
+++ src/test/scala/org/broadinstitute/gpp/poolq3/testutil/package.scala
@@ -10,14 +10,14 @@ import java.nio.file.{Path => JPath}
 import scala.io.Source
 import scala.util.Using
 
-import cats.effect.{Async, Resource}
+import cats.effect.Resource
 import fs2.io.file.Files
 
 package object testutil {
 
   def contents(p: JPath): String = Using.resource(Source.fromFile(p.toFile))(_.mkString)
 
-  def tempFile[F[_]: Async](prefix: String, suffix: String): Resource[F, JPath] =
+  def tempFile[F[_]: Files](prefix: String, suffix: String): Resource[F, JPath] =
     Files[F].tempFile(None, prefix, suffix, None).map(_.toNioPath)
 
 }
