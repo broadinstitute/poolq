f661d6579253be42019b5ab8f2601a3ea71faafc
Mark Tomko <mtomko@broadinstitute.org>
2023-08-29 08:16:23 -0700

    Port simple tests to munit

--- src/test/scala/org/broadinstitute/gpp/poolq3/barcode/KeyMaskTest2.scala
+++ src/test/scala/org/broadinstitute/gpp/poolq3/barcode/KeyMaskTest2.scala
@@ -5,23 +5,22 @@
  */
 package org.broadinstitute.gpp.poolq3.barcode
 
-import org.scalatest.flatspec.AnyFlatSpec
-import org.scalatest.matchers.should.Matchers._
+import munit.FunSuite
 
 /** This class provides additional tests for the KeyMask that are not found in the FISHR codebase from which KeyMask and
   * its primary test class were lifted. We will try not to modify the copied test classes to make subsequent updates
   * from FISHR easier. Instead, new PoolQ-specific tests will live here.
   */
-class KeyMaskTest2 extends AnyFlatSpec {
+class KeyMaskTest2 extends FunSuite {
 
-  "KeyMask.apply" should "construct the correct key mask from a pattern" in {
+  test("construct the correct key mask from a pattern") {
     //                 0        1         2
     //                 12345678901234567890123456789
     val km0 = KeyMask("caccgNNNNNnnnnnnnnnttacaNNNNN")
 
     // parsing should work how we expect
-    km0.keyRanges should be(Seq(KeyRange(5, 9), KeyRange(24, 28)))
-    km0.keyLengthInBases should be(10)
+    assertEquals(km0.keyRanges, Seq(KeyRange(5, 9), KeyRange(24, 28)))
+    assertEquals(km0.keyLengthInBases, 10)
   }
 
 }
