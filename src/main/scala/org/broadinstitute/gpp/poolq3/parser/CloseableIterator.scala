25e475c7613c60bdc35fc7c2195ea172456b08ad
Mark Tomko <mtomko@broadinstitute.org>
2023-09-01 14:11:31 -0700

    Add stub implementation of CloseableIterator

--- src/main/scala/org/broadinstitute/gpp/poolq3/parser/CloseableIterator.scala
+++ src/main/scala/org/broadinstitute/gpp/poolq3/parser/CloseableIterator.scala
@@ -8,3 +8,15 @@ package org.broadinstitute.gpp.poolq3.parser
 import java.io.Closeable
 
 abstract class CloseableIterator[A] extends Iterator[A] with Closeable
+
+object CloseableIterator {
+
+  /** A convenience implementation used for testing */
+  def ofList[A](xs: List[A]): CloseableIterator[A] = new CloseableIterator[A] {
+    val iter = xs.iterator
+    override def close(): Unit = ()
+    override def hasNext: Boolean = iter.hasNext
+    override def next(): A = iter.next()
+  }
+
+}
