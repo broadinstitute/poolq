ae553dba62781c9af65793b55eaaa8971fd09145
Mark Tomko <mtomko@broadinstitute.org>
2023-10-29 20:36:48 -0700

    Function to parse barcodes at the end of a read ID

---
+++ src/main/scala/org/broadinstitute/gpp/poolq3/barcode/Dmuxed.scala
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) 2022 The Broad Institute, Inc. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+package org.broadinstitute.gpp.poolq3.barcode
+
+object Dmuxed {
+
+  private[barcode] def barcodeFromId(length: Int): String => Option[FoundBarcode] = {
+    val regex = s"@.*[^ACGTN]([ACGTN]{$length})$$".r
+    _ match {
+      case regex(barcode) => Some(FoundBarcode(barcode.toCharArray, 0))
+      case _              => None
+    }
+  }
+
+}
---
+++ src/test/scala/org/broadinstitute/gpp/poolq3/barcode/DmuxedTest.scala
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) 2022 The Broad Institute, Inc. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+package org.broadinstitute.gpp.poolq3.barcode
+
+import munit.FunSuite
+
+class DmuxedTest extends FunSuite {
+
+  test("extracting a barcode with Ns from an illumina read") {
+    assertEquals(
+      Dmuxed.barcodeFromId(8)("@A01379:680:HC37HDRX3:1:2101:1163:1000 1:N:0:GGNGNANT"),
+      Some(FoundBarcode("GGNGNANT".toCharArray, 0))
+    )
+  }
+
+  test("extracting a barcode with no Ns from an illumina read") {
+    assertEquals(
+      Dmuxed.barcodeFromId(8)("@A01379:680:HC37HDRX3:1:2101:3224:1000 1:N:0:AAATGCGA"),
+      Some(FoundBarcode("AAATGCGA".toCharArray, 0))
+    )
+  }
+
+  test("ignore a barcode-like sequence that's too long") {
+    assertEquals(Dmuxed.barcodeFromId(8)("@A01379:680:HC37HDRX3:1:2101:3224:1000 1:N:0:AAATGCGAGG"), None)
+  }
+
+  test("ignore a barcode-like sequence that's too short") {
+    assertEquals(Dmuxed.barcodeFromId(8)("@A01379:680:HC37HDRX3:1:2101:3224:1000 1:N:0:TGCGAGG"), None)
+  }
+
+}
