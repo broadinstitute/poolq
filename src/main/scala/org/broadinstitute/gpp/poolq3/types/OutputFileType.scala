14034d2960cd8ad79b9add957ee8e74a3b373682
Mark Tomko <mtomko@broadinstitute.org>
2024-02-08 14:59:19 -0800

    Add output file type

--- src/main/scala/org/broadinstitute/gpp/poolq3/types/OutputFileType.scala
+++ src/main/scala/org/broadinstitute/gpp/poolq3/types/OutputFileType.scala
@@ -8,6 +8,7 @@ package org.broadinstitute.gpp.poolq3.types
 trait OutputFileType extends Product with Serializable
 case object CountsFileType extends OutputFileType
 case object QualityFileType extends OutputFileType
+case object ConditionBarcodeCountsSummaryFileType extends OutputFileType
 case object LogNormalizedCountsFileType extends OutputFileType
 case object BarcodeCountsFileType extends OutputFileType
 case object CorrelationFileType extends OutputFileType
