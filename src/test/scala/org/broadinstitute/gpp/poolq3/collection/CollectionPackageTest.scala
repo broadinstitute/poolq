c79309c1f069e45b605d87b49d75752e5d9dd0a0
Mark Tomko <mtomko@broadinstitute.org>
2024-02-20 08:20:57 -0800

    Update copyright

--- src/test/scala/org/broadinstitute/gpp/poolq3/collection/CollectionPackageTest.scala
+++ src/test/scala/org/broadinstitute/gpp/poolq3/collection/CollectionPackageTest.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2022 The Broad Institute, Inc. All rights reserved.
+ * Copyright (c) 2024 The Broad Institute, Inc. All rights reserved.
  *
  * SPDX-License-Identifier: BSD-3-Clause
  */
